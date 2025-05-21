55582838e2439de83ed0726ebf4c98afef14421e
Brian <brianliu1319@gmail.com>
2025-05-20 17:04:32 -0700

    some html

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "./style/style.css"
 const createHeader = function () {
     const header = document.querySelector("#header");
 
-    header.textContent = "WHAT THE BROWN";
+    header.textContent = "AXOS BANK";
 }
 
 createHeader();
