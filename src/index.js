60168745d894e3ada5886168371c95659d902544
Brian <brianliu1319@gmail.com>
2025-05-22 16:51:08 -0700

    more css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import "./style/style.css"
+import "./style/style.css";
+import plus from "./imgs/plus.svg";
+import plus2 from "./imgs/plus2.svg";
 
 
 
-createHeader();
+// button hover
+
+console.log("hello!!");
+const icon = document.querySelector(".panelTitle.add");
+
+icon.addEventListener("mouseenter", () => {
+  icon.src = plus2;
+});
+
+icon.addEventListener("mouseleave", () => {
+  icon.src = plus;
+});
