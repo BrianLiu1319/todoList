e1d7a22e3d0a130c7577533db707dfeeb6cb849a
Brian <brianliu1319@gmail.com>
2025-05-22 15:19:16 -0700

    some css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import "./style/style.css"
 
-const createHeader = function () {
-    const header = document.querySelector("#header");
 
-    header.textContent = "AXOS BANK";
-}
 
 createHeader();
