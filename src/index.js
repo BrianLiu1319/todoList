0b3a4cf9a0eeb319bb077c909520173ae0e9a626
Brian <brianliu1319@gmail.com>
2025-05-22 17:22:06 -0700

    EOD -> just some notes for the future

--- src/index.js
+++ src/index.js
@@ -5,14 +5,36 @@ import plus2 from "./imgs/plus2.svg";
 
 
 // button hover
+buttonHover = () => {
+    const icon = document.querySelector(".panelTitle.add");
 
-console.log("hello!!");
-const icon = document.querySelector(".panelTitle.add");
+    icon.addEventListener("mouseenter", () => {
+    icon.src = plus2;
+    });
 
-icon.addEventListener("mouseenter", () => {
-  icon.src = plus2;
-});
+    icon.addEventListener("mouseleave", () => {
+    icon.src = plus;
+    });
+}
 
-icon.addEventListener("mouseleave", () => {
-  icon.src = plus;
-});
+buttonHover();
+
+// need to make to do card :
+/**
+ * 
+ *  To do card will have : subject, details , date, edit, delete, favorite
+ * 
+ * How to store by each project :
+ * 
+ * projects contains a list of projects :
+ * 
+ * projects = [[proj1], [proj2], ...]
+ * 
+ * each proj contains a list of tasks
+ * 
+ * 
+ * Clicking Home buttons will display based on sorting (forEach projects) and create a list of projs to display
+ */
+
+subjectSelector 
+
